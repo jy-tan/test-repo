7443c75f0243378f5f1bb02b18d7e5571017ee18
JY Tan <jy8230@gmail.com>
2025-01-09 12:32:59 -0800

    Commit

---
+++ utils/format_text.py
@@ -0,0 +1,11 @@
+import re
+
+def make_camelcase(text):
+    words = text.lower().split()
+    return words[0] + ''.join(word.capitalize() for word in words[1:])
+
+def remove_special_characters(text):
+    return re.sub(r'[^a-zA-Z0-9\s]', '', text)
+
+def format_currency(number):
+    return f"${number:,.2f}"
